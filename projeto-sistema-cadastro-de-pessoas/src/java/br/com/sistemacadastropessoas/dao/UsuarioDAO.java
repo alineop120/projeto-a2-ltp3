7e3eee09247372624169f045c2961d7a6f9ce799
Aline Pinho <alineveira2@gmail.com>
2025-04-08 14:58:40 -0300

    criando br.com.sistemacadastropessoas

---
+++ projeto-sistema-cadastro-de-pessoas/src/java/br/com/sistemacadastropessoas/dao/UsuarioDAO.java
@@ -0,0 +1,25 @@
+package br.com.etb.dao;
+
+import java.util.List;
+import br.com.sistemacadastropessoas.model.Usuario;
+
+/**
+ *
+ * @author 364975
+ */
+public class UsuarioDAO {
+
+    public List<Usuario> listarTodos() {
+    	return null;
+    }
+
+    public void inserir(Usuario usuario) {
+    }
+
+    public void atualizar(Usuario usuario) {
+    }
+
+    public void deletar(int id) {
+    }
+
+}
--- projeto-sistema-cadastro-de-pessoas/src/java/dao/UsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-/**
- *
- * @author 364975
- */
-public class UsuarioDAO {
-    
-}
