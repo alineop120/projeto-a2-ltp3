7ef659f82ae7d93fbe76b87d7f552bca1f7b314a
Aline Pinho <alineveira2@gmail.com>
2025-04-15 15:36:13 -0300

    Update script-criacao-cadastrobd.sql

--- mySQL/script-criacao-cadastrobd.sql
+++ mySQL/script-criacao-cadastrobd.sql
@@ -1,17 +1,20 @@
--- Criar um banco de dados (bd)
-create database cadastrobd;
+-- Criar o banco de dados
+CREATE DATABASE cadastrobd;
 
--- Utilizar o bd
-use cadastrobd;
+-- Selecionar o banco de dados
+USE cadastrobd;
 
--- Criar table do bd
-create table usuarios (
-	id int not null auto_increment primary key,
-    nome varchar(255),
-    email varchar(255),
-    senha varchar(255),
-    acesso int
+-- Criar a tabela 'usuarios'
+CREATE TABLE usuarios (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    acesso INT NOT NULL
 );
 
--- Excluir um bd
--- DROP database cadastrobd;
+-- Visualizar todos os dados da tabela 'usuarios'
+SELECT * FROM usuarios;
+
+-- Excluir o banco de dados (cuidado!)
+-- DROP DATABASE cadastrobd;
